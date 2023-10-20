0a3f43f12080211660a1bce3b7571fa07c27811d
Daria Nikulina <daryanickulina@yandex.ru>
2023-10-20 14:12:44 +0400

    feat: Tooltip

---
+++ src/Components/Tooltip/Tooltip.jsx
@@ -0,0 +1,21 @@
+import "./Tooltip.css";
+import React from "react";
+
+export default function Tooltip({ tooltipText }) {
+  const [isTooltipOpen, setIsTooltipOpen] = React.useState(false);
+
+  function toggleTooltip() {
+    isTooltipOpen ? setIsTooltipOpen(false) : setIsTooltipOpen(true);
+  }
+  return (
+    <div className="tooltip relative" onClick={toggleTooltip}>
+      <p
+        className={`${
+          !isTooltipOpen && "hidden"
+        } tooltip__text py-1 px-2 bg-[#41434E] m-0 absolute w-[266px] top-7 z-10 rounded`}
+      >
+        {tooltipText}
+      </p>
+    </div>
+  );
+}
