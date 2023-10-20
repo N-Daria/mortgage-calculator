77ada72ed7c6c9995f9d18c902e4eb07f5e74237
Daria Nikulina <daryanickulina@yandex.ru>
2023-10-20 13:59:56 +0400

    fix: Clue props

--- src/Components/Сlue/Сlue.jsx
+++ src/Components/Сlue/Сlue.jsx
@@ -1,9 +1,11 @@
 import "./Clue.css";
 
-export default function Clue({ text }) {
+export default function Clue({ text, isError }) {
   return (
-    <div className="hidden bg-red w-full rounded-md mt-3">
-      <p className="clue__text py-[6px] pr-[21px] pl-[32px] relative">{text}</p>
+    <div className={`${isError ? "" : "hidden"} bg-red w-full rounded-md mt-3`}>
+      <p className="clue__text py-[6px] pr-[21px] pl-[32px] relative m-0">
+        {text}
+      </p>
     </div>
   );
 }
